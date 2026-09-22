128c7d3b4b1e8df0d4592196621807afc5a405af
maribvidal <marianobazalarvidal@gmail.com>
2026-09-22 16:51:15 -0300

    Cambié el título y la descripción de la página Swagger.

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    const config = new DocumentBuilder()
-    .setTitle('App example')
-    .setDescription('The app API description')
+    .setTitle('Página del Swagger')
+    .setDescription('La descripción para la API de SearchServiceSpec')
     .setVersion('1.0')
     .addTag('model')
     .build();
