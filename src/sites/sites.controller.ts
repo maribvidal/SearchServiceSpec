a4102544cac154c69a32cb834d568e9d17c77056
maribvidal <marianobazalarvidal@gmail.com>
2026-09-22 18:16:50 -0300

    Agregué una descripción breve para cada endpoint existente.

--- src/sites/sites.controller.ts
+++ src/sites/sites.controller.ts
@@ -9,16 +9,19 @@ export class SitesController {
   constructor(private readonly sitesService: SitesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear un nuevo sitio.' })
   create(@Body() createSiteDto: CreateSiteDto) {
     return this.sitesService.create(createSiteDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todos los sitios creados.' })
   findAll() {
     return this.sitesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener un sitio específico por su id.' })
   findOne(@Param('id') id: string) {
     return this.sitesService.findOne(id);
   }
@@ -30,11 +33,13 @@ export class SitesController {
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Modificar la información de un sitio específico por medio de su id. (No se pueden modificar los siguientes campos: nombre, url, userId)'})
   update(@Param('id') id: string, @Body() updateSiteDto: UpdateSiteDto) {
     return this.sitesService.update(id, updateSiteDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un sitio específico por medio de su id.' })
   remove(@Param('id') id: string) {
     return this.sitesService.remove(id);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,26 +9,31 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear un nuevo usuario.' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todos los usuarios creados.' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener un sitio específico por su id.' })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Modificar la información de un usuario específico por medio de su id.'})
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un usuario específico por medio de su id.' })
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
